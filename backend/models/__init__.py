7cadf026e82de75cff6fe7a715014a10fd863629
halalh4x0r <mohamed.hadi@student.moringaschool.com>
2025-11-25 12:45:54 +0300

    installed backend dependencies

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,8 +1,6 @@
-from flask_sqlalchemy import SQLAlchemy
+from config import db
 
 from .user import User
 from .event import Event
 from .comment import Comment
 from .rsvp import RSVP
-
-from config import db  # or however you import db
